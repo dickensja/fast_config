850534d88a00085929df77d411f7e40808c36c28
Joe Dickens <dickens.ja@gmail.com>
2016-10-12 12:43:47 -0700

    add FastConfigEnv

--- lib/fast_config.rb
+++ lib/fast_config.rb
@@ -24,3 +24,28 @@ module FastConfig
     downcase
   end
 end
+
+module FastConfigEnv
+  def self.extended(main_class)
+    configs = YAML.load_file(File.expand_path("config/#{snake_case(main_class)}.yml"))
+    configs[ENV['RAILS_ENV']].each{ |k,v| main_class.set(k.to_sym, v) }
+    super
+  end
+
+  @settings = {}
+  def settings
+    @settings ||= {}
+  end
+
+  def set(key, val)
+    settings[key] = val
+  end
+
+  def self.snake_case(camel_cased_word)
+    camel_cased_word.to_s.gsub(/::/, '_').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
